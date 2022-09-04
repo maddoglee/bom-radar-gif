f98d83573e830f493334eff9cc7d9730ea8cf5ae
Lee <caroleandlee@gmail.com>
2022-09-04 15:46:39 +1000

    Code with customised static local background (non-BOM)

--- bomradargif_FTP.py
+++ bomradargif_FTP.py
@@ -5,7 +5,7 @@ import io
 import ftplib
 from PIL import Image
 
-product_id = 'IDR713' # The ID for our radar image - IDR713 = Sydney
+product_id = 'IDR034' # The ID for our radar image - IDR713 = Sydney
 frames = [] # List to store the images
 
 # The layers that we want in the order from bottom to top
