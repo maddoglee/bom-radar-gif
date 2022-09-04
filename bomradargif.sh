e0fa663b5a1be4da402cef0cf6ace04230ee0eb1
Lee <caroleandlee@gmail.com>
2022-09-04 22:55:37 +1000

    Code with customised static local background (non-BOM)

--- bomradargif.sh
+++ bomradargif.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 export PYTHONPATH='/usr/lib/python37.zip', '/usr/lib/python3.7', '/usr/lib/python3.7/lib-dynload', '/home/pi/.local/lib/python3.7/site-packages', '/usr/local/lib/python3.7/dist-packages', '/usr/lib/python3/dist-packages'
 
-echo $(date) >> bash_cron_log.txt
-/usr/bin/python3 /home/pi/bom-radar-gif/bomradargif_STATIC.py >> bash_cron_log.txt
+/usr/bin/python3 /home/pi/bom-radar-gif/bomradargif_STATIC.py
