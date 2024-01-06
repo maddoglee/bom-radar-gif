b4574b310ef3c52c518b8f93292afdd7217aaf7b
maddoglee <caroleandlee@gmail.com>
2024-01-06 13:47:12 +1100

    Update bomradargif.sh
    Introduced check to see if the script is already running before launching again. 
    I found the BOM FTP site times out and then the script keeps loading constantly.

--- bomradargif.sh
+++ bomradargif.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 #export PYTHONPATH='/usr/lib/python37.zip' '/usr/lib/python3.7' '/usr/lib/python3.7/lib-dynload', '/home/pi/.local/lib/python3.7/site-packages', '/usr/local/lib/python3.7/dist-packages', '/usr/lib/python3/dist-packages'
 
-/usr/bin/python3 /home/pi/bom-radar-gif/bomradargif_STATIC.py
+process_name="python3"
+
+if pgrep -x "$process_name" > /dev/null; then
+    echo "The process $process_name is running."
+else
+    echo "The process $process_name is not running."
+   /usr/bin/python3 /home/pi/bom-radar-gif/bomradargif_STATIC.py
+fi
